c231635be44d01b348f99f5cb415af7fe376fd23
papasfritas05 <kcrodriguezj@gmail.com>
2023-10-19 12:28:13 -0500

    VS Code

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+        intVector test = new intVector(-1000);
+
+        System.out.println("Size: " + test.size());
+
+        for(int i = 0; i < test.size(); i++) {
+
+            
+            }
+          catch(IllegalArgumentException ex) {
+            System.out.println("thatsnIlligal");
+        }
+          catch(ArithmeticException ex) {
+            System.out.println("bad math");
+        }
+          catch(Exception ex) {
+            System.out.println("Generic Exception");
+        }
+        
+        }
+    }
+}
---
+++ intVector.java
@@ -0,0 +1,18 @@
+public class intVector{
+
+    private final static int INITIAL_CAPACITY = 10;
+    private int[] arr = new int [ INITIAL_CAPACITY];
+    private int size = 0;
+
+    //default connstructor
+    public intVector() { }
+    
+    public intVector(int capacity) { 
+        if(capacity < 0){
+            throw new IllegalArgumentException();
+        }
+        arr = new int(capacity);
+    }
+
+    public int size() {return size; }
+}
