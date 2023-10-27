32a761cf941765f7ba860fff76dbc476f7d1433a
papasfritas05 <kcrodriguezj@gmail.com>
2023-10-27 11:38:26 -0500

    copying a copy

--- intVector.java
+++ intVector.java
@@ -15,4 +15,60 @@ public class IntVector{
     }
 
     public int size() {return size; }
+    private void testBounds(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void add(int n){
+
+        arr[size++] = n;
+    }
+
+    private void resize(){
+        if(size >= arr.length -1){
+
+            int[] temp = new int [1 + arr.length * 2];
+
+
+            for(int i =0; i < size; i++){
+                temp[i] = arr[i];
+            }
+
+            arr = temp;
+        }
+    }
+
+    public int get(int index){
+        testBounds(index);
+        return arr[index];
+    }
+
+    public void clear() {
+        arr = new int[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public boolean add(int index,int value){
+        for(int i = 0; i < size; i++){
+            arr[index] = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remove (int value){
+
+    }
+
+    @Override
+    public String toString(){
+        if(size == 0)return "[]";
+        String t = "[" + arr [0];
+        for(int i = 0; i < size; i++){
+            t += "," + arr[i];
+        }
+        return t + "[" ;
+        }
 }
