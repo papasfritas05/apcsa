2bfd4dfb4d8fe9b81ee65d90c94358c3638e508d
papasfritas05 <kcrodriguezj@gmail.com>
2023-10-31 12:29:48 -0500

    Brant helped

--- Main.java
+++ Main.java
@@ -1,21 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            IntVector test = new IntVector(-1000);
-
-            System.out.println("Size: " + test.size());
-
-            for(int i = 0; i < test.size(); i++) {
-
+            IntVector test = new IntVector(0);
+            
+            for (int i = 0; i < 20; i++) {
+                test.add(i*2);
             }
+            System.out.println(test);
+
+            test.add(17,100);
+            System.out.println(test);
+
+            //System.out.println(test);
+
+            //test.add(50,50);
+            System.out.println(test);
+
+            //test.remove(3);
+            //System.out.println(test);
+            
         } catch(IllegalArgumentException ex) {
-            System.out.println("thatsnIlligal");
-        }
-          catch(ArithmeticException ex) {
+            System.out.println("That's illegal!");
+        } catch(ArithmeticException ex) {
             System.out.println("bad math");
+        } catch(Exception ex) {
+            System.out.println("Generic exception");
         }
-          catch(Exception ex) {
-            System.out.println("Generic Exception");
-        }
+        
     }
 }
--- intVector.java
+++ intVector.java
@@ -1,38 +1,31 @@
-public class IntVector{
-
+public class IntVector {
+    
     private final static int INITIAL_CAPACITY = 10;
-    private int[] arr = new int[ INITIAL_CAPACITY];
+    private int[] arr = new int[INITIAL_CAPACITY];
     private int size = 0;
 
-    //default connstructor
     public IntVector() { }
 
-    public IntVector(int capacity) { 
-        if(capacity < 0){
+    public IntVector(int capacity) {
+        if (capacity < 0) {
             throw new IllegalArgumentException();
         }
         arr = new int[capacity];
     }
 
-    public int size() {return size; }
-    private void testBounds(int index){
-        if(index < 0 || index >= size){
+    public int size() { return size; }
+
+    private void testBounds(int index) {
+        if(index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
     }
 
-    public void add(int n){
+    private void tryResize() {
+        if(size >= arr.length - 1) {
+            int[] temp = new int[1 + arr.length * 2];
 
-        arr[size++] = n;
-    }
-
-    private void resize(){
-        if(size >= arr.length -1){
-
-            int[] temp = new int [1 + arr.length * 2];
-
-
-            for(int i =0; i < size; i++){
+            for (int i = 0; i < size; i++) {
                 temp[i] = arr[i];
             }
 
@@ -40,35 +33,54 @@ public class IntVector{
         }
     }
 
-    public int get(int index){
+    public void add(int value) {
+        tryResize();
+        arr[size++] = value;
+    }
+
+    public boolean add(int index, int value) {
         testBounds(index);
-        return arr[index];
-    }
-
-    public void clear() {
-        arr = new int[INITIAL_CAPACITY];
-        size = 0;
-    }
-
-    public boolean add(int index,int value){
-        for(int i = 0; i < size; i++){
-            arr[index] = value;
+        int temp = arr[index];
+        arr[index] = value;
+        add(arr[size - 1]);
+        for (int i = size - 2; i > index; i--) {
+            arr[i+1] = arr[i];
+        }
+        arr[index + 1] = temp;
+        if (arr[index] == value) {
             return true;
         }
         return false;
     }
 
-    public boolean remove (int value){
+    public boolean remove(int value) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == value) {
+                for (int j = i; j < size - 1; j++) {
+                    arr[j] = arr[j+1];
+                }
+                size--;
+                tryResize();
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public void set(int index, int value) {
+        testBounds(index);
+        arr[index] = value;
     }
 
     @Override
-    public String toString(){
-        if(size == 0)return "[]";
-        String t = "[" + arr [0];
-        for(int i = 0; i < size; i++){
-            t += "," + arr[i];
+    public String toString() {
+        if(size == 0) {
+            return "[]";
         }
-        return t + "[" ;
+        String t = "[" + arr[0];
+        for(int i = 1; i < size; i++) {
+            t += ", " + arr[i];
         }
+        return t + "]";
+    }
 }
